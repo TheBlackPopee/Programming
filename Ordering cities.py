daaaeadfd61e16443256841fdff760839199cef2
TheBlackPopee <62901979+TheBlackPopee@users.noreply.github.com>
2020-04-12 20:13:27 +0100

    Add files via upload

---
+++ Ordering cities.py
@@ -0,0 +1,8 @@
+cities = ['London', 'Paris', 'Berlin', 'Moscow', 'Lisbon']
+cities2 = ['London', 'Paris', 'Berlin', 'Moscow', 'Lisbon']
+
+cities.sort(reverse=True)
+print('Sorted list in Descending order):', cities)
+
+cities2.sort()
+print('Sorted list in Ascending order):', cities2)
